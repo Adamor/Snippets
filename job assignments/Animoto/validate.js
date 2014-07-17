c9e7446541972129c8fb822c7ac1a4a124130545
Arthur Bouie <arthurbouie@gmail.com>
2014-07-17 08:57:44 -0400

    tweak

--- job assignments/Animoto/validate.js
+++ job assignments/Animoto/validate.js
@@ -3,13 +3,13 @@ var validateForm = function(){
     var valid = true; // Set inital validation to true
     var emailAddy = document.forms.input.email.value; // Variable to hold email address value
     var passwordVal = document.forms.input.password.value; // Variable to hold password value
-    var emailError = document.getElementById("emailNotValid");
-    var passError = document.getElementById("passNotValid");
+    var emailError = document.getElementById("emailNotValid"); // Variable for to grab the span element for email message
+    var passError = document.getElementById("passNotValid"); // Variable for to grab the span element for password message
 
 
     // Regex for Email validation
     var regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var regPass = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/; //lowercase letters and Numbers
+    var regPass = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/; // Numbers, lowercase, uppercase, special char, between 8-64 chars long.
 
     // Basic Email validation start 
     if(regEmail.test(emailAddy) === false){
@@ -30,6 +30,6 @@ var validateForm = function(){
         toggleClass(passNotValid, 'hidden');
         valid = false;
     }
-    
+
     return valid;
 };
